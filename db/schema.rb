d794e2d81e5a19f87c769a858fafec4e8d634cd4
guillermo <gl874391@gmail.com>
2019-12-11 00:21:40 +0000

    course creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_215621) do
+ActiveRecord::Schema.define(version: 2019_12_10_233909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "cost"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string "email", default: "", null: false
