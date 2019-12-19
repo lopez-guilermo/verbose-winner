ed56db8a64c15fd783c21a8c48b9d2ff34a22d39
guillermo <gl874391@gmail.com>
2019-12-19 15:33:34 +0000

    ordering lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
 	belongs_to :section
 	 has_many :images
+
+	 mount_uploader :video, VideoUploader
+
+	 include RankedModel
+	 ranks :row_order, with_same: :section_id
+
+
 end
